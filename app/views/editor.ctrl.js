dccf82a7583d1da61000b842b11ad57274290ce1
lorenzo <l.romagnoli@arduino.cc>
2016-11-26 19:29:19 +0800

    modified file name

---
+++ app/views/editor.ctrl.js
@@ -0,0 +1,13 @@
+write.controller('editorCtrl', ['$rootScope', '$scope',
+  function($rootScope, $scope) {
+
+    $scope.doc={}
+    $scope.doc.title="newFile";
+    $scope.doc.sourceCode=". . . ";
+
+    sharejs.open('home', 'text', function(error, doc) {
+       doc.attach_textarea(pad);
+   });
+
+  }
+]);
