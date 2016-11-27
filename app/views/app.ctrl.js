b4747431fde2f92d47acdd05e2fa5dc53cf851be
lorenzo <l.romagnoli@arduino.cc>
2016-11-27 12:56:23 +0800

    basic flow is now working

--- app/views/app.ctrl.js
+++ app/views/app.ctrl.js
@@ -3,6 +3,27 @@ write.controller('appCtrl', ['$rootScope', '$scope', '$location',
 
     $scope.isDocOpen=false;
 
+    $scope.doc={};
+    $scope.doc.title =" ";
+    $scope.doc.sourceCode=". . . ";
+
+
+    angular.element(document).ready(function () {
+      var location=$location.url().substring(1);;
+
+      if (location==""){
+
+      }else{
+        $scope.isDocOpen=true;
+        $scope.changeLocation(location);
+      }
+
+
+    });
+
+
+
+
     $scope.changeLocation=function(url){
       console.log ($location.url());
       console.log (url);
@@ -13,6 +34,7 @@ write.controller('appCtrl', ['$rootScope', '$scope', '$location',
 
     $scope.openFile=function(fileName){
       $scope.isDocOpen=true;
+      $scope.doc.title=fileName;
 
       sharejs.open(fileName, 'text', function(error, doc) {
          doc.attach_textarea(pad);
--- app/views/editor.ctrl.js
+++ app/views/editor.ctrl.js
@@ -1,9 +1,7 @@
 write.controller('editorCtrl', ['$rootScope', '$scope',
   function($rootScope, $scope) {
 
-    $scope.doc={}
-    $scope.doc.title="newFile";
-    $scope.doc.sourceCode=". . . ";
+    
 
   }
 ]);
