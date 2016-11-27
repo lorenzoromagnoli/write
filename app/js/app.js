9f712e174d659fa03bcb42c9d1bf9cb05269f62e
lorenzo <l.romagnoli@arduino.cc>
2016-11-27 12:11:24 +0800

    giving sense to the all flow

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,12 @@ var write = angular.module('write', [
 									'ngAnimate',
                   'btford.markdown'
 
-									]);
+									],function($locationProvider){
+                      $locationProvider.html5Mode({
+                        enabled: true,
+                        requireBase: false
+                      });
+                  });
 
 
 write.controller('appCtrl', ['$scope', '$rootScope', function($scope, $rootScope){
